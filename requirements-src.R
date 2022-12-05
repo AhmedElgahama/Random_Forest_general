049d790d63538353d825ca05b32cc44853282361
AhmedElgahama <40144555+AhmedElgahama@users.noreply.github.com>
2022-12-05 20:49:42 +0200

    Update requirements-src.R

--- requirements-src.R
+++ requirements-src.R
@@ -1,5 +1,5 @@
 pkgs <- c(
-    'xgboost', 'tictoc', 'imputeTS', 'pROC', 'caTools', 'CatEncoders', 'Metrics', 'forcats'
+    'tictoc', 'imputeTS', 'pROC', 'caTools', 'CatEncoders', 'Metrics', 'forcats'
 )
 
 install.packages(pkgs)
